88868d6b552b28ec82e73f97cdcf0227deb7f2e1
Kelly O'Brien <kelly@kismetdevelopment.com>
2020-09-03 17:28:01 -0700

    Initial Commit

---
+++ OrderUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderUp
+//
+//  Created by Kelly O'Brien on 9/3/20.
+//  Copyright © 2020 Kismet Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

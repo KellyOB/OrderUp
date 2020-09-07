4ad4ebe21a7c1d5b7ae57976004ab07dde7c1e82
Kelly O'Brien <kelly@kismetdevelopment.com>
2020-09-07 16:29:24 -0700

    added cart item model

---
+++ OrderUp/Model/CartItem.swift
@@ -0,0 +1,14 @@
+//
+//  CartItem.swift
+//  OrderUp
+//
+//  Created by Kelly O' on 9/5/20.
+//  Copyright © 2020 Kismet Development. All rights reserved.
+//
+
+import Foundation
+
+struct CartItem {
+    var item: MenuItem
+    var quantity: Int
+}
